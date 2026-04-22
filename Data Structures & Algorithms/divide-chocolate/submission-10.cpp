50097ab780d3b10ad3632b1a523bfbabdfa0272f
Luke Xiaoyuan Zeng <150103968+lukezeng123@users.noreply.github.com>
2026-04-22 18:28:39 +1000

    Add: divide-chocolate - submission-10

---
+++ Data Structures & Algorithms/divide-chocolate/submission-10.cpp
@@ -0,0 +1,34 @@
+class Solution {
+
+    bool possible(vector<int>& sweetness, int mid, int k) {
+        int cur = 0;
+        int segments = 0;
+        for (int i = 0; i < sweetness.size(); i++) {
+            cur += sweetness[i];
+            if (cur >= mid) {
+                cur = 0;
+                segments++;
+            }
+        }
+        return segments >= k + 1;
+    }
+public:
+    int maximizeSweetness(vector<int>& sweetness, int k) {
+        // binary search
+
+        int l = 0;
+        int r = INT_MAX;
+        int mid;
+        int ans = 0;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (possible(sweetness, mid, k)) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
