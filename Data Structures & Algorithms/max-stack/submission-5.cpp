af2cfbb49a80d5eddd2c39925b50bec60017a767
Luke Xiaoyuan Zeng <150103968+lukezeng123@users.noreply.github.com>
2026-04-22 19:58:56 +1000

    Add: max-stack - submission-5

---
+++ Data Structures & Algorithms/max-stack/submission-5.cpp
@@ -0,0 +1,50 @@
+class MaxStack {
+    // popMax
+    // list as stack
+    list<int> st;
+    // track max with multiset and list iterator
+    map<int, stack<list<int>::iterator>> m;
+public:
+    MaxStack() {
+    }
+    
+    void push(int x) {
+        st.push_front(x);
+        m[x].push(st.begin());
+    }
+    
+    int pop() {
+        int x = st.front();
+        m[x].pop();
+        if (m[x].empty()) m.erase(x);
+        st.pop_front();
+        return x;
+    }
+    
+    int top() {
+        return st.front();
+    }
+    
+    int peekMax() {
+        return prev(m.end())->first;
+    }
+    
+    int popMax() {
+        int x = prev(m.end())->first;
+        auto it = m[x].top();
+        st.erase(it);
+        m[x].pop();
+        if (m[x].empty()) m.erase(x);
+        return x;
+    }
+};
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack* obj = new MaxStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->peekMax();
+ * int param_5 = obj->popMax();
+ */
